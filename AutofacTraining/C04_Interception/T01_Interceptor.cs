72f26aead9042cf64a694670669141381a6faf2b
Michał Włodarczyk <mwlodarczyk@forcom.com.pl>
2017-06-11 23:23:06 +0200

    Interceptor

---
+++ AutofacTraining/C04_Interception/T01_Interceptor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Autofac;
+using Autofac.Extras.DynamicProxy2;
+using Castle.DynamicProxy;
+using NUnit.Framework;
+
+namespace AutofacTraining.C04_Interception
+{
+	class T01_Interceptor : ContainerTest
+	{
+		public class ProductService
+		{
+			public ProductService()
+			{
+				Console.WriteLine("ProductService ctor");
+			}
+
+			public virtual void Save()
+			{
+				Console.WriteLine("ProductService Save");
+				Thread.Sleep(500);
+			}
+		}
+
+		public class InvocationTimeLogger : IInterceptor
+		{
+			public void Intercept(IInvocation invocation)
+			{
+				var stopwatch = new Stopwatch();
+				stopwatch.Start();
+				
+				invocation.Proceed();
+
+				stopwatch.Stop();
+				Console.WriteLine("{0}.{1} took {2}",
+					invocation.TargetType.Name, invocation.Method.Name, stopwatch.Elapsed);
+			}
+		}
+
+		protected override void RegisterComponents(ContainerBuilder builder)
+		{
+			builder.RegisterType<ProductService>()
+				.EnableClassInterceptors()
+				.InterceptedBy(typeof(InvocationTimeLogger));
+
+			builder.RegisterType<InvocationTimeLogger>();
+		}
+
+		[Test]
+		public void SaveWithInterception()
+		{
+			var productService = Resolve<ProductService>();
+			productService.Save();
+		}
+	}
+}
--- AutofacTraining/Properties/AssemblyInfo.cs
+++ AutofacTraining/Properties/AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("27ee0e43-cebb-4166-b1f6-30e59320cd58")]
