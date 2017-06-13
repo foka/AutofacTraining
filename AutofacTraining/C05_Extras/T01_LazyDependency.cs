b0cf0d0d9de4d726e87f4935c40337bee3616d10
Michał Włodarczyk <mwlodarczyk@forcom.com.pl>
2017-06-13 09:53:32 +0200

    Lazy<T>

---
+++ AutofacTraining/C05_Extras/T01_LazyDependency.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Autofac;
+using NUnit.Framework;
+
+namespace AutofacTraining.C05_Extras
+{
+	// Kolega na szkoleniu spytał czy Autofac obsługuje serwis Lazy<T>
+	// tak jak Func<T>. Poniższy test pokazuje, że Lazy<T> działa!
+	// Oczywiście, jak to przy Lazy, komponent tworzony jest tylko raz.
+	class T01_LazyDependency
+	{
+		class ProductService
+		{
+			public ProductService(Lazy<ProductDao> lazyProductDao)
+			{
+				this.lazyProductDao = lazyProductDao;
+				Console.WriteLine("ProductService ctor");
+			}
+
+			public void Save()
+			{
+				lazyProductDao.Value.Save();
+			}
+
+			private readonly Lazy<ProductDao> lazyProductDao;
+		}
+
+		class ProductDao
+		{
+			public ProductDao()
+			{
+				Console.WriteLine("ProductDao ctor");
+			}
+
+			public void Save()
+			{
+				Console.WriteLine("ProductDao Save");
+			}
+		}
+
+		[Test]
+		public void CreateProductService()
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterType<ProductService>();
+			builder.RegisterType<ProductDao>();
+
+			var container = builder.Build();
+
+			var productService = container.Resolve<ProductService>();
+			productService.Save();
+			productService.Save();
+
+//			Output:
+//			ProductService ctor
+//			ProductDao ctor
+//			ProductDao Save
+//			ProductDao Save
+		}
+	}
+}
